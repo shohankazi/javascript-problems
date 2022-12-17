52992457ff6f7f64b9026308d77497119374a77b
shohankazi <shohankazi012@gmail.com>
2022-12-18 00:16:53 +0600

    problem solving

---
+++ problem-5.js
@@ -0,0 +1,5 @@
+/* Calculate return of the shopkeeper after buying some fruits */
+var momGiven = 1000;
+var fruitCost = 700;
+var shopkeeperReturn = momGiven - fruitCost;
+console.log(shopkeeperReturn);
